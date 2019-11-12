405b05c48948469e3b994a68b59bb1559bd97e1a
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-11-12 16:03:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     if params[:username] == "" || params[:password] == ""
-      redirect "/falure"
+      redirect "/failure"
     else
       User.create(:username => params[:username], :password => params[:password])
       redirect "/login"
